e9a90ac9115544b5dd4bc4dae8dca29a44bc2449
Franco Bontorin <bontorin@amazon.com>
2023-05-27 14:45:20 -0400

    new vpc

--- main.tf
+++ main.tf
@@ -27,27 +27,15 @@ locals {
 ################################################################################
 
 resource "aws_vpc" "this" {
-  count = local.create_vpc ? 1 : 0
+  count       = var.create_vpc ? 1 : 0
+  cidr_block  = var.vpc_cidr
 
-  cidr_block          = var.use_ipam_pool ? null : var.cidr
-  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
-  ipv4_netmask_length = var.ipv4_netmask_length
-
-  assign_generated_ipv6_cidr_block     = var.enable_ipv6 && !var.use_ipam_pool ? true : null
-  ipv6_cidr_block                      = var.ipv6_cidr
-  ipv6_ipam_pool_id                    = var.ipv6_ipam_pool_id
-  ipv6_netmask_length                  = var.ipv6_netmask_length
-  ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
-
-  instance_tenancy                     = var.instance_tenancy
-  enable_dns_hostnames                 = var.enable_dns_hostnames
-  enable_dns_support                   = var.enable_dns_support
-  enable_network_address_usage_metrics = var.enable_network_address_usage_metrics
 
   tags = merge(
-    { "Name" = var.name },
-    var.tags,
-    var.vpc_tags,
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
   )
 }
 
